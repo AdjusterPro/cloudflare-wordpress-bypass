9460f2500f3140e5e27bfaa115f0fe00a0a75b1a
Ben Dunlap <ben@adjusterpro.com>
2020-03-06 11:54:31 -0600

    add templateized wrangler.toml and ignore the real one since it contains CF account-specific stuff //bdunlap

--- webpack.development.js
+++ webpack.development.js
@@ -1,6 +1,6 @@
 module.exports = {
   "target": "webworker",
-  "devtool": "cheap-module-source-map",
+  "devtool": "cheap-module-source-map", // avoid 'eval': Workers environment doesn't allow it
   "entry": "./index.js",
   "mode": "development"
 }
